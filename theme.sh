fd903cfb4372bfe2bbb54d2d95ed6c3b44073454
Grace <ghan0004@student.monash.edu>
2018-02-18 10:27:17 +0000

    update

--- theme.sh
+++ theme.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-CONFIG_FILE="/home/han/src.txt"
-THEME_FILE="/home/han/themes.grace"
+CONFIG_FILE="/home/han/Personal/termtheme/test/src.txt"
+THEME_FILE="/home/han/Personal/termtheme/test/themes.grace"
 usage="$0 [theme] [optional flags]"
 args=$#
 
 function change_theme {
-	#grep config file for defined symb
-	#define starting line num
-	# delete from starting line to end of config file,
+
+	start_line=$(grep -nr themeterm.start $CONFIG_FILE | awk -F: '{print $1}')
+	end_line=$(grep -nr themeterm.end $CONFIG_FILE | awk -F: '{print $1}')
+
+	start_line=$((start_line + 1))
+	end_line=$((end_line - 1))
+
+	# delete from starting line to end line
+	sed -i.bak -e "${start_line}, ${end_line}d" $CONFIG_FILE
+	
+	# parse theme file
+	theme_start_line=$(grep -nr $1 $THEME_FILE | awk -F: '{print $1}')
+	sed -n 's/> ${$1}/'  
 }
 
 
