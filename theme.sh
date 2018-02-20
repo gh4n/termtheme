41ed54153aa18ce30ee36d7196b42de61f4756af
Grace Han <ghan0004@student.monash.edu>
2018-02-21 01:08:04 +1100

    update

--- theme.sh
+++ theme.sh
@@ -6,7 +6,8 @@ usage="$0 [theme] [optional flags]"
 args=$#
 
 function change_theme {
-
+	
+	# parse config file for theme flags
 	start_line=$(grep -nr themeterm.start $CONFIG_FILE | awk -F: '{print $1}')
 	end_line=$(grep -nr themeterm.end $CONFIG_FILE | awk -F: '{print $1}')
 
@@ -16,12 +17,33 @@ function change_theme {
 	# delete from starting line to end line
 	sed -i.bak -e "${start_line}, ${end_line}d" $CONFIG_FILE
 	
-	# parse theme file
-	theme_start_line=$(grep -nr $1 $THEME_FILE | awk -F: '{print $1}')
+	# parse theme file for theme name
+	OUTPUT=$(grep -nr $1 $THEME_FILE | awk -F: '{print $1}')
+	
+	if [[ $OUTPUT ]]; then
+		theme_start_line=$OUTPUT
+	else
+		echo theme not found!
+	fi
+
 	# get endline, if returns nothing assume end of file 
-	OUTPUT=$(tail -n +$theme_start_line $THEME_FILE | grep -i ">")
+	OUTPUT=$(tail -n +$theme_start_line $THEME_FILE | grep -i -n ">" | awk -F: 'NR==1{print $1}')
+	if [[ $OUTPUT ]]; then
+		theme_end_line=$OUTPUT
+	else
+		theme_end_line=$(wc -l < $THEME_FILE)
+	fi
 
+	# parse config file again for line to paste code
+	start_line=$(grep -nr themeterm.start $CONFIG_FILE | awk -F: '{print $1}')
 
+	# copy and paste code
+	theme=$(sed -n "${theme_start_line}, ${theme_end_line}p" $THEME_FILE)
+	sed '${start_line} a ${theme}' $CONFIG_FILE
+	
+
+	# refresh config file
+	xrdb $CONFIG_FILE
 
 }
 
